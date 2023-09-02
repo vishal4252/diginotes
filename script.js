ec8882188e801336e7e12fa2c535ff6adc95c494
vishal <vp7390734@gmail.com>
2023-09-02 22:46:57 +0530

    try to fix pdf download

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 AOS.init(
     duration = 500
 );
+
+document.addEventListener('DOMContentLoaded', function () {
+    const pdfLink = document.getElementById('pdfLink');
+
+    pdfLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        window.open(pdfLink.getAttribute('href'), '_parent');
+    });
+});
