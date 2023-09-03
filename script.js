ff5a0fe99ffa16703060504b82f855465dab968e
vishal <vp7390734@gmail.com>
2023-09-03 20:39:50 +0530

    change dark color preference

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 AOS.init(
     duration = 500
 );
-
-function openPDFInParentTab(url) {
-    var win = window.open(url, "_parent");
-    win.focus();
-}
-
-// Attach the function to the onclick event of the PDF link
-var pdfLink = document.querySelector("a[href*='.pdf']");
-pdfLink.onclick = function () {
-    openPDFInParentTab(this.href);
-};
